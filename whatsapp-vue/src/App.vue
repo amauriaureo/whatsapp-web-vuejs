60d965a0a2770dc12cf66617ebb5536a22b4098c
amauriaureo <amauriaureo@gmail.com>
2021-10-22 21:39:15 -0300

    estilizacao css

--- whatsapp-vue/src/App.vue
+++ whatsapp-vue/src/App.vue
@@ -4,9 +4,12 @@
       <div class="container">
         <div class="columns">
           <div class="column is-3 lista-de-conversas">
-            
+            <div class="barra-superior" />
           </div>
-          <div class="column">
+          <div class="column conversa-ativa">
+            <div class="barra-superior">
+              <span>Usuário</span>
+            </div>
             
           </div>
         </div>
@@ -19,5 +22,27 @@
 .columns{
   min-height: 800px;
   box-shadow: 0 3rem 3rem -1rem rgba(10,10,10,.2);
+}
+
+.column{
+  padding: 0;
+}
+.lista-de-conversas{
+  background-color: white;;
+}
+.conversa-ativa{
+  background: #E5DDD5;
+}
+.barra-superior{
+  margin: 0;
+  height: 50px;
+  background: #EDEDED;
+  border-right: 1px solid #E1E1E1;
+  border-bottom: 1px solid rgb(200, 200, 200);
+}
+.barra-superior span {
+  line-height: 50px;
+  margin-left: 25px;
+  font-weight: 500;
 }
 </style>
